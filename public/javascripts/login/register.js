cde9398bf5b3f112b3764071fc2f08f73be8adf6
Gunvor Nilsson <gunvor@behovsbo.se>
2019-09-23 21:27:19 +0200

    bryptjs also in register

--- public/javascripts/login/register.js
+++ public/javascripts/login/register.js
@@ -1,32 +1,31 @@
 const db = require('../../../db/database.js');
 const reg = require('../status.js');
-const saltRounds = 10;
+const bcrypt = require('bcryptjs');
 
 function hashbcrypt(req, res) {
-    const bcrypt = require('bcrypt');
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(req.body.value, salt, function(err, hash) {
+            if (err) {
+                let obj = reg.reterror(500, "/register", "bcrypt error");
 
-    bcrypt.hash(req.body.value, saltRounds, function(err, hash) {
-        if (err) {
-            let obj = reg.reterror(500, "/register", "bcrypt error");
+                return res.status(500).json(obj);
+            }
 
-            return res.status(500).json(obj);
-        }
+            db.run("INSERT INTO users (email, password) VALUES (?, ?)",
+                req.body.column, hash, (err) => {
+                    if (err) {
+                        let obj = reg.reterror(500, "/register", err.message);
 
-        db.run("INSERT INTO users (email, password) VALUES (?, ?)",
-            //req.body.email, hash, (err) => {
-            req.body.column, hash, (err) => {
-                if (err) {
-                    let obj = reg.reterror(500, "/register", err.message);
-
-                    return res.status(500).json(obj);
-                }
-
-                res.status(201).json({
-                    data: {
-                        message: "User " + req.body.column + " registered with: ." + hash
+                        return res.status(500).json(obj);
                     }
+
+                    res.status(201).json({
+                        data: {
+                            message: "User " + req.body.column + " registered with: ." + hash
+                        }
+                    });
                 });
-            });
+        });
     });
 }
 
