408b5884206aa9d95701ee23884f7dc420ff77b6
Gunvor Nilsson <gunvor@behovsbo.se>
2019-09-25 09:06:57 +0200

    added file

---
+++ public/javascripts/login/insert.js
@@ -0,0 +1,23 @@
+const db = require('../../../db/database.js');
+const reg = require('../status.js');
+
+function insert(req, res, params) {
+    db.run("INSERT INTO users (email, password) VALUES (?, ?)",
+        params, (err) => {
+            if (err) {
+                let obj = reg.reterror(500, "register", err.message);
+
+                return res.status(500).json(obj);
+            }
+
+            res.status(201).json({
+                data: {
+                    message: "User " + params[0] + " registered with: ." + params[1]
+                }
+            });
+        });
+}
+
+module.exports = {
+    insert: insert
+};
--- public/javascripts/login/register.js
+++ public/javascripts/login/register.js
@@ -1,30 +1,17 @@
-const db = require('../../../db/database.js');
-const reg = require('../status.js');
+const reg = require('../status');
+const insert = require('./insert');
 const bcrypt = require('bcryptjs');
 
 function hashbcrypt(req, res) {
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(req.body.value, salt, function(err, hash) {
             if (err) {
-                let obj = reg.reterror(500, "/register", "bcrypt error");
+                let obj = reg.reterror(500, "register", "bcrypt error");
 
                 return res.status(500).json(obj);
             }
 
-            db.run("INSERT INTO users (email, password) VALUES (?, ?)",
-                req.body.column, hash, (err) => {
-                    if (err) {
-                        let obj = reg.reterror(500, "/register", err.message);
-
-                        return res.status(500).json(obj);
-                    }
-
-                    res.status(201).json({
-                        data: {
-                            message: "User " + req.body.column + " registered with: ." + hash
-                        }
-                    });
-                });
+            insert.insert(req, res, [req.body.column, hash]);
         });
     });
 }
