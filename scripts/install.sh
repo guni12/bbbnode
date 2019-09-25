5cf5b434384149de014cc6e770a202267a802735
Gunvor Nilsson <gunvor@behovsbo.se>
2019-09-25 07:55:50 +0200

    package-lock back

--- scripts/install.sh
+++ scripts/install.sh
@@ -81,8 +81,8 @@ cd /home/pi/bbbnode
 sudo python3 /home/pi/bbbnode/public/scripts/spot/checkfile.py
 sudo python3 /home/pi/bbbnode/public/scripts/spot/movefiles.py
 
-tmp=$(mktemp)
-jq 'del(.dependencies.rpio, .dependencies.sqlite3)' /home/pi/bbbnode/package.json > "$tmp" && mv "$tmp" /home/pi/bbbnode/package.json
+#tmp=$(mktemp)
+#jq 'del(.dependencies.rpio, .dependencies.sqlite3)' /home/pi/bbbnode/package.json > "$tmp" && mv "$tmp" /home/pi/bbbnode/package.json
 
 sudo npm install ds18b20-raspi -g
 sudo npm install node-pre-gyp -g
@@ -90,8 +90,8 @@ sudo npm install node-gyp -g
 sudo npm install pm2 -g
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi
 npm install --production # installs modules without devDependencies
-npm install sqlite3 --build-from-source
-npm install rpio
+#npm install sqlite3 --build-from-source --sqlite=/usr
+#npm install rpio
 
 echo "installation ok, the system will restart" | sudo tee -a /boot/config.txt
 sudo reboot
